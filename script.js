5c546b36d2ae0c5ce4081b7bb6f99e5e81f5e165
SvetkaKo <skovakina@gmail.com>
2022-05-20 11:39:13 -0700

    add reveal sections

--- script.js
+++ script.js
@@ -15,18 +15,41 @@ window.addEventListener(
     const st = window.pageYOffset || document.documentElement.scrollTop;
     if (st > lastScrollTop) {
       console.log(window.pageYOffset);
+      console.log(document.documentElement.scrollTop);
       nav.classList.add('nav-hide');
-      nav.classList.remove('shadow');
-      //   console.log('down');
-      // }
-      // if (window.pageYOffset <= 0) {
-      //   nav.classList.remove('sticky', 'shadow');
+      nav.classList.remove('sticky');
+      // console.log('down');
+    } else if (document.documentElement.scrollTop <= 70) {
+      // console.log('top');
+      nav.classList.add('nav-back');
     } else {
-      nav.classList.remove('nav-hide');
-      nav.classList.add('sticky', 'shadow');
+      nav.classList.remove('nav-hide', 'nav-back', 'sticky');
+      nav.classList.add('sticky');
       //   console.log('up');
     }
     lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
   },
   false
 );
+
+////////Reveal Sections
+const revaelSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return; //return fo avoiding bags
+  entry.target.classList.remove('section--hidden');
+  //stop observer
+  observer.unobserve(entry.target);
+};
+
+const allSections = document.querySelectorAll('.section');
+
+const sectionObserver = new IntersectionObserver(revaelSection, {
+  root: null,
+  threshold: 0.2,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
